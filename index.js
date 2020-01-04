552b7ea52d5a48360ab6d8fd4e7d1cd36e07dae9
Yaroslav R <rostanets@gmail.com>
2020-01-04 21:13:23 +0200

    upd

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const http = require('http');
+const ws = require('ws');
+
+Array.prototype.removeEl = function(el) {
+  const index = this.findIndex(item => el === item);
+  if (index) this.slice(index, 1);
+};
+Array.prototype.allExcept = function(el) {
+  return this.filter(item => item !== el);
+};
+const sockets = [];
+
+const wss = new ws.Server({ port: 3001 });
+wss.on('connection', ws => {
+  sockets.push(ws);
+  console.log('CONNECTED: ', sockets);
+  ws.on('message', message => messageHandler(message, ws));
+  ws.onclose = () => {console.log('CONNECTED: ', sockets); sockets.removeEl(ws)};
+});
+
+const messageHandler = (message, ws) => {
+  console.log('CONNECTED: ', sockets);
+  sockets.allExcept(ws).forEach( socket => socket.send(message));
+};
+
+
+
 const PostRouter = require('./PostRouter');
 
 global.status = {};
@@ -12,13 +38,11 @@ const GetRouter = (url, res) => {
   }
 };
 
-
-
 http.createServer(function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   req.method === 'GET' ? GetRouter(req.url, res) : PostRouter(req.url, req, res);
-}).listen(80); //the server object listens on port 8080
+}).listen(3000); //the server object listens on port 8080
 
